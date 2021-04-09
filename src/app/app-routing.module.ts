55a088557ac15fb5e4905842e8e39cf3730b8320
João Guilherme Monteiro Reis <jguilherme58@gmail.com>
2021-04-08 21:35:32 -0300

    ajuste nas rotas e começando os produtos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConteudoComponent } from './view/conteudo/conteudo.component'
 import { HeaderComponent } from './components/template/header/header.component';
+import { ProdutoComponent } from './view/produto/produto.component'
 
 const routes: Routes = [
  
-{path:"",component :HeaderComponent}
+{path:"",component :ConteudoComponent},
+{path:"produto",component:ProdutoComponent}
  
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { ConteudoComponent } from './view/conteudo/conteudo.component'
+import { ConteudoComponent } from './view/conteudo/conteudo.component';
+import { ProdutoComponent } from './view/produto/produto.component'
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import { ConteudoComponent } from './view/conteudo/conteudo.component'
     FooterComponent,
     NavigationComponent,
     ConteudoComponent,
+    ProdutoComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,8 @@ import { ConteudoComponent } from './view/conteudo/conteudo.component'
     MatListModule,
     MatButtonModule,
     MatIconModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule
   ],
   
   providers: [],
